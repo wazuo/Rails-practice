8922b9c8b9044ab4388e3feb3dd1071795edbee1
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-08 06:44:39 +0000

    [finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,11 +5,28 @@ class TodolistsController < ApplicationController
   def create
    list =List.new(list_params)
    list.save
-   redirect_to'/top'
+   redirect_to todolist_path(list.id)
   end
+  def index
+     @lists =List.all
+  end
+
+  def show
+     @list =List.find(params[:id])
+  end
+  def edit
+     @list =List.find(params[:id])
+  end
+  def update
+     list =List.find(params[:id])
+     list.update(list_params)
+     redirect_to todolist_path(list.id)
+      
+  end
+
  private
  def list_params
-   params.require(:list).permit(:title, :bpdy)
+   params.require(:list).permit(:title, :body)
  end
- 
+
 end
